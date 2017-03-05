4f4bf334d14e337edbba90c653e7a84e3119d703
Sunil kumar <sunilssaharan@gmail.com>
2017-03-06 04:01:00 +0530

    changed settings and urls for new app

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/', include('webapp.urls')),
+	url(r'^polls/', include('polls.urls')),
     url(r'^', include('perso.urls')),
 
 ]

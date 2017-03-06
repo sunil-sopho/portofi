38bb0668f9d5de5f5c074b56ec98c096bdab2b41
Sunil kumar <sunilssaharan@gmail.com>
2017-03-06 20:17:59 +0530

    made site working connected webpages yet individual page for blog is remaining and use of polls left

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
-from __future__ import unicode_literals
-
 from django.db import models
+#from django.utils.encoding import python_2_unicode_compatible
 
-class post(models.Model):
+#@python_2_unicode_compatible 
+class Post(models.Model):
 	title = models.CharField(max_length=150)
 	body = models.TextField()
 	date = models.DateTimeField()
 	
-	def__unicode__(self):
-		return self.tile
+	def __str__(self):
+		return self.title
 		
 
 # Create your models here.

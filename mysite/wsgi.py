fd1b6a2bb5097f5d920deac0d8066ce0ac46811e
Sunil kumar <sunilssaharan@gmail.com>
2017-03-07 00:33:24 +0530

    added proc

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,3 +16,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

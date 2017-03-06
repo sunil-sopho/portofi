eaa61be4b018f0da6c7249455ffb0a066e9cb36f
Sunil kumar <sunilssaharan@gmail.com>
2017-03-07 02:00:57 +0530

    changed wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
+from django.core import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 

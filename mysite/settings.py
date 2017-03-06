ae148a711887bc7d1b6f24051c4ce8fe00984ae4
Sunil kumar <sunilssaharan@gmail.com>
2017-03-06 23:38:18 +0530

    changed setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-	import dj_database_url
-	db_from_env = dj_database_url.config(conn_max_age=500)
-	DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 	
 	
 STATIC_URL = '/static/'
